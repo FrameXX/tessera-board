3b6cac0b167c8ae8841e75bf32a8b26ae866aae4
FrameXX <jirkakral978@gmail.com>
2023-09-15 16:20:57 +0200

    Add FastButton component

---
+++ src/components/FastButton.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+import Icon from "./Icon.vue";
+
+const props = defineProps({
+  title: { type: String, required: true },
+  iconId: { type: String, required: true },
+});
+
+defineEmits(["click"]);
+</script>
+
+<template>
+  <div class="fast-button-wrapper">
+    <button
+      @click="$emit('click')"
+      :aria-label="props.title"
+      :title="props.title"
+    >
+      <Icon :icon-id="props.iconId" />
+    </button>
+    <div class="title">{{ props.title }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "../partials/mixins";
+
+.fast-button-wrapper {
+  @include flex-center(inline-flex);
+  text-align: center;
+  flex-direction: column;
+  width: 60px;
+  height: 100px;
+  padding: var(--spacing-small);
+
+  > button {
+    @include no-shrink;
+    padding: var(--spacing-tiny);
+  }
+
+  > .title {
+    height: 100%;
+    font-size: var(--font-size-tiny);
+  }
+}
+</style>
